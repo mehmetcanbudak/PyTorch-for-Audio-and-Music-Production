45c1f2a8dcf8ea1ef03224fb806f227c04250757
Mehmmetcan Budak <mehmetcanbudak@gmail.com>
2024-01-30 15:16:07 +0300

    created the model

--- train.py
+++ train.py
@@ -10,7 +10,25 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
-BATCH_SIZE = 128
+BATCH_SIZE = 128  # number of data points in each batch
+
+
+class FeedForwardNet(nn.Module):  # inherit from nn.Module
+    def __init__(self):  # constructor
+        super().__init__()  # call parent constructor
+        self.flatten = nn.Flatten()  # flatten the image tensors
+        self.dense_layers = nn.Sequential(  # create a sequence of layers
+            nn.Linear(28 * 28, 256),  # 1st layer
+            nn.ReLU(),  # activation function
+            nn.Linear(256, 10),  # 2nd layer
+        )
+        self.softmax = nn.Softmax(dim=1)  # apply softmax function
+
+    def forward(self, input_data):  # forward pass
+        flattened_data = self.flatten(input_data)  # flatten the input data
+        logits = self.dense_layers(flattened_data)  # pass data to dense layers
+        predictions = self.softmax(logits)  # apply softmax function
+        return predictions  # return predictions
 
 
 def download_mnist_datasets():
@@ -26,7 +44,7 @@ def download_mnist_datasets():
         train=False,  # don't train dataset
         transform=ToTensor(),  # convert data to tensor
     )
-    return train_data, validation_data
+    return train_data, validation_data  # return datasets
 
 
 if __name__ == "__main__":
@@ -36,3 +54,8 @@ if __name__ == "__main__":
 
     # create data loader for the train set
     train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE)
+
+    # build model
+    device = "cuda" if torch.cuda.is_available() else "cpu"  # check if GPU is available
+    print(f"Using {device} device")
+    feed_forward_net = FeedForwardNet().to(device)  # create model and send it to device
