f9e403f8ed731f942474fe2e46a14e2ef0219981
Mehmmetcan Budak <mehmetcanbudak@gmail.com>
2024-01-30 15:02:33 +0300

    created a dataloader

--- train.py
+++ train.py
@@ -10,6 +10,8 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+BATCH_SIZE = 128
+
 
 def download_mnist_datasets():
     train_data = datasets.MNIST(
@@ -31,3 +33,6 @@ if __name__ == "__main__":
     # download MNIST dataset
     train_data, _ = download_mnist_datasets()
     print("MNIST dataset downloaded successfully")
+
+    # create data loader for the train set
+    train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE)
