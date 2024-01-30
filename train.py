696eab94682389f60ff558ae16a72ca8975f5c0a
Mehmmetcan Budak <mehmetcanbudak@gmail.com>
2024-01-30 15:29:45 +0300

    train the model

--- train.py
+++ train.py
@@ -11,6 +11,9 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 BATCH_SIZE = 128  # number of data points in each batch
+EPOCHS = 10  # number of times to pass through the whole dataset
+# LEARNING_RATE = 1e-3  # learning rate
+LEARNING_RATE = 0.001  # learning rate
 
 
 class FeedForwardNet(nn.Module):  # inherit from nn.Module
@@ -47,6 +50,32 @@ def download_mnist_datasets():
     return train_data, validation_data  # return datasets
 
 
+def train_one_epoch(model, data_loader, loss_fn, optimizer, device):  # train one epoch
+    for input, targets in data_loader:
+        inputs, targets = inputs.to(device), targets.to(device)  # send data to device
+
+        # calculate loss
+        predictions = model(inputs)  # forward pass
+        loss = loss_fn(predictions, targets)  # calculate loss
+
+        # bacpropogate loss and update weights
+        optimiser.zero_grad()  # reset gradients
+        loss.backward()  # backpropogate loss
+        optimiser.step()  # update weights
+
+    print(f"Loss: {loss.item()}")
+
+
+def train(model, data_loader, loss_fn, optimizer, device, epochs):  # train model
+    for i in range(epochs):  # train for number of epochs
+        print(f"Epoch {i+1}")  # print epoch number
+        train_one_epoch(
+            model, data_loader, loss_fn, optimizer, device
+        )  # train one epoch
+        print("---------------------------")  # print seperator
+    print("Training is done.")  # print finished message
+
+
 if __name__ == "__main__":
     # download MNIST dataset
     train_data, _ = download_mnist_datasets()
@@ -59,3 +88,17 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"  # check if GPU is available
     print(f"Using {device} device")
     feed_forward_net = FeedForwardNet().to(device)  # create model and send it to device
+
+    # instantiate loss function and optimiser
+    loss_fn = nn.CrossEntropyLoss()  # instantiate loss function
+    # optimiser = torch.optim.SGD(
+    #     feed_forward_net.parameters(), lr=1e-3
+    # )  # instantiate optimiser with SGD
+    optimiser = torch.optim.Adam(
+        feed_forward_net.parameters(), lr=LEARNING_RATE
+    )  # instantiate optimiser with Adam
+
+    # train model
+    train(
+        feed_forward_net, train_dataloader, loss_fn, optimiser, device, EPOCHS
+    )  # train model
