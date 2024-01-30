f529c52ca4158947271fd6b4c05182f68a8ec1f1
Mehmmetcan Budak <mehmetcanbudak@gmail.com>
2024-01-31 00:15:39 +0300

    Added a basic cli.py file for later use

---
+++ cli.py
@@ -0,0 +1,19 @@
+import argparse
+
+from cnn import CNNNetwork
+from inference import predict
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Audio CNN")
+    parser.add_argument("input_file", help="Path to input file.")
+    parser.add_argument("output_file", help="Path to output file.")
+    args = parser.parse_args()
+
+    model = ""
+    result = ""
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
