16583bbf94ff8891aebd364cbf8410c923d74b80
Mehmmetcan Budak <mehmetcanbudak@gmail.com>
2024-01-30 19:49:25 +0300

    Pre-processing Audio with Different Durations

--- urbansounddataset.py
+++ urbansounddataset.py
@@ -4,17 +4,24 @@ import os  # for file path
 import pandas as pd  # for data processing
 import torch
 import torchaudio  # for audio processing
-from torch.utils.data import ConcatDataset, Dataset  # for creating custom dataset
+from torch.utils.data import ConcatDataset  # for creating custom dataset
+from torch.utils.data import Dataset
 
 
 class UrbanSoundDataset(Dataset):
     def __init__(
-        self, annotations_file, audio_dir, transformation, target_sample_rate
+        self,
+        annotations_file,
+        audio_dir,
+        transformation,
+        target_sample_rate,
+        num_samples,
     ):  # constructor
         self.annotations = pd.read_csv(annotations_file)  # load annotations file
         self.audio_dir = audio_dir  # set audio directory
         self.transformation = transformation  # set transformation
         self.target_sample_rate = target_sample_rate  # set target sample rate
+        self.num_samples = num_samples  # set number of samples
 
     def __len__(self):  # return length of dataset
         return len(self.annotations)  # return number of samples in dataset
@@ -27,7 +34,27 @@ class UrbanSoundDataset(Dataset):
         signal = self._resample_if_necessary(signal, sr)  # resample signal if necessary
         signal = self._mix_down_if_necessary(signal)  # mix down signal if necessary
         signal = self.transformation(signal)  # apply transformation
+        signal = self._cut_if_necessary(signal)  # cut signal if necessary
+        signal = self._right_pad_if_necessary(signal)  # right pad signal if necessary
         return signal, label  # return signal and label
+
+    def _cut_if_necessary(self, signal):  # cut signal if necessary
+        # signal -> Tensor -> (1, num_samles) -> (1, 50000) -> (1, 22050)
+        if signal.shape[1] > self.num_samples:  # check signal length
+            signal = signal[:, : self.num_samples]  # cut signal
+        return signal
+
+    def _right_pad_if_necessary(self, signal):  # right pad signal if necessary
+        length_signal = signal.shape[1]  # get signal length
+        if length_signal < self.num_samples:  # check signal length
+            num_missing_samples = (
+                self.num_samples - length_signal
+            )  # calculate number of missing samples
+            last_dim_padding = (0, num_missing_samples)  # create padding
+            signal = torch.nn.functional.pad(
+                signal, last_dim_padding
+            )  # pad signal on last dimension
+        return signal
 
     def _resample_if_necessary(self, signal, sr):  # resample signal if necessary
         if sr != self.target_sample_rate:  # check sample rate
@@ -54,13 +81,13 @@ class UrbanSoundDataset(Dataset):
 
 
 if __name__ == "__main__":
-    ANNOTAIONS_FILE = (
-        "/Users/mehmetcanbudak/Projects/Mehmetcan/PyTorch_Audio/UrbanSound8K/UrbanSound8K.csv"
-    )
+    ANNOTAIONS_FILE = "/Users/mehmetcanbudak/Projects/Mehmetcan/PyTorch_Audio/UrbanSound8K/UrbanSound8K.csv"
     AUDIO_DIR = (
         "/Users/mehmetcanbudak/Projects/Mehmetcan/PyTorch_Audio/UrbanSound8K/audio/"
     )
-    SAMPLE_RATE = 16000  # sample rate of audio file
+    # SAMPLE_RATE = 16000 # sample rate of audio file
+    SAMPLE_RATE = 22050  # sample rate of audio file
+    NUM_SAMPLES = 22050  # number of samples
 
     mel_spectrogram = torchaudio.transforms.MelSpectrogram(
         sample_rate=SAMPLE_RATE,  # sample rate of audio file
@@ -71,7 +98,7 @@ if __name__ == "__main__":
     # ms = mel_spectogram(signal)
 
     usd = UrbanSoundDataset(
-        ANNOTAIONS_FILE, AUDIO_DIR, mel_spectrogram, SAMPLE_RATE
+        ANNOTAIONS_FILE, AUDIO_DIR, mel_spectrogram, SAMPLE_RATE, NUM_SAMPLES
     )  # instantiate dataset
     print(f"There are {len(usd)} samples in the dataset.")  # print length of dataset
     signal, label = usd[0]  # get first sample
